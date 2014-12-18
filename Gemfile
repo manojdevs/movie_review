fa745ff62926c4233770e7374c39190a29cc45ef
manojdevs <manojselvamdev@gmail.com>
2014-12-18 22:26:15 +0530

    bootstrap modifications

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'devise'
 gem 'paperclip'
+gem 'bootstrap-sass', '~> 3.2.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :movies
   root 'movies#index'
+  devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
